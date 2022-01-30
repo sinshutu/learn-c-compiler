6b977482cd953478bdb12f43277644857b00cabd
NaotoSuzuki <suzuki.z2o.naoto@gmail.com>
2022-01-30 20:35:20 +0900

    step6

--- 9cc.c
+++ 9cc.c
@@ -153,11 +153,13 @@ Token *tokenize(char *p) {
 }
 
 Node *mul();
+Node *unary();
 Node *primary();
 
 // BNF
 // expr    = mul ("+" mul | "-" mul)*
-// mul     = primary ("*" primary | "/" primary)*
+// mul     = unary ("*" unary | "/" unary)*
+// unary = ("+" | "-")? primary
 // primary = num | "(" expr ")"
 Node *expr() {
   Node *node = mul();
@@ -173,16 +175,24 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for(;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 Node *primary() {
